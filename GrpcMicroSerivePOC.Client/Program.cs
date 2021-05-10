ce97a8ba4db9205250fb43d786920c70ff828a85
javidombronsky <javier.dombronsky@w3americas.com>
2021-05-10 17:54:56 -0300

    Add project files.

---
+++ GrpcMicroSerivePOC.Client/Program.cs
@@ -0,0 +1,42 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcMicroServicePOC.Protos;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcMicroSerivePOC.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Equipo.EquipoClient(channel);
+
+            using var equiposStream = client.GetAllTeams(new EmptyRequest());
+            while (await equiposStream.ResponseStream.MoveNext())
+            {
+                var equipo = equiposStream.ResponseStream.Current;
+                Console.WriteLine($"Equipo: {equipo.Nombre}");
+                Console.WriteLine($"Titulos: {equipo.Titulos}");
+                Console.WriteLine($"Descensos: {equipo.Descensos}");
+                Console.WriteLine($"Grande: {equipo.EsGrande}");
+                Console.WriteLine($"Ubicacion: {equipo.Ubicacion}");
+                Console.WriteLine("----------------------------");
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Ingrese un Id");
+                int id = int.Parse(Console.ReadLine());
+                var equipo = await client.EquipoInfoAsync(new EquipoRequest { Id = id });
+                Console.WriteLine($"Equipo: {equipo.Nombre}");
+                Console.WriteLine($"Titulos: {equipo.Titulos}");
+                Console.WriteLine($"Descensos: {equipo.Descensos}");
+                Console.WriteLine($"Grande: {equipo.EsGrande}");
+                Console.WriteLine($"Ubicacion: {equipo.Ubicacion}");
+            }
+
+        }
+    }
+}
---
+++ GrpcMicroServicePOC/Services/EquipoService.cs
@@ -0,0 +1,80 @@
+﻿using Grpc.Core;
+using GrpcMicroServicePOC.Protos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GrpcMicroServicePOC.Services
+{
+    public class EquipoService : Equipo.EquipoBase
+    {
+
+        public override Task<EquipoReply> EquipoInfo(EquipoRequest request, ServerCallContext context)
+        {
+            var equipos = MisEquipos();
+
+            if (request.Id < equipos.Count)
+            {
+                return Task.FromResult(equipos[request.Id]);
+            }
+
+            throw new System.Exception("Bad Request");
+        }
+
+        public override async Task GetAllTeams(EmptyRequest request, IServerStreamWriter<EquipoReply> responseStream, ServerCallContext context)
+        {
+            var equipos = MisEquipos();
+            foreach (var equipo in equipos)
+            {
+                await responseStream.WriteAsync(equipo);
+            }
+        }
+
+        private List<EquipoReply> MisEquipos()
+        {
+            var boca = new EquipoReply
+            {
+                Descensos = 0,
+                EsGrande = true,
+                Nombre = "Boca Juniors",
+                Titulos = 70,
+                Ubicacion = EquipoReply.Types.Ubicacion.Capital
+            };
+            var racing = new EquipoReply
+            {
+                Descensos = 1,
+                EsGrande = false,
+                Nombre = "Racing",
+                Titulos = 37,
+                Ubicacion = EquipoReply.Types.Ubicacion.BuenosAires
+            };
+            var river = new EquipoReply
+            {
+                Descensos = 1,
+                EsGrande = true,
+                Nombre = "River Plate",
+                Titulos = 66,
+                Ubicacion = EquipoReply.Types.Ubicacion.Capital
+            };
+            var independiente = new EquipoReply
+            {
+                Descensos = 1,
+                EsGrande = true,
+                Nombre = "Independiente",
+                Titulos = 45,
+                Ubicacion = EquipoReply.Types.Ubicacion.BuenosAires
+            };
+            var rosario = new EquipoReply
+            {
+                Descensos = 2,
+                EsGrande = false,
+                Nombre = "Club Rosario Puerto Belgrano",
+                Titulos = 8,
+                Ubicacion = EquipoReply.Types.Ubicacion.Interior
+            };
+            return new List<EquipoReply>
+            {
+                boca, river, racing, independiente,rosario
+            };
+        }
+    }
+}
